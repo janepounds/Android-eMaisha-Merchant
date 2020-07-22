7223e6d72fc620868ecdf1851602a5af8f291717
wamozo cosmas <wcosmas75@gmail.com>
2020-07-22 12:52:10 +0300

    Merchant Auth

---
+++ app/src/main/java/com/cabral/emaishamerchant/auth/Registration.java
@@ -0,0 +1,13 @@
+package com.cabral.emaishamerchant.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
