22fb135e400a7fff637e3cf22f82c7efcf951264
wamozo cosmas <wcosmas75@gmail.com>
2020-09-16 15:49:58 +0300

    Wallet UI

---
+++ app/src/main/java/com/cabral/emaishamerchantsapp/wallet/SettlementDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.cabral.emaishamerchantsapp.wallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettlementDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settlement_details);
+    }
+}
